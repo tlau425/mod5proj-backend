9c0694c09549c82b50de92c0d54346d710da4aa8
Tommy Lau <tlau425@yahoo.com>
2019-07-09 11:18:54 -0400

    create

--- app/controllers/api/v1/warranties_controller.rb
+++ app/controllers/api/v1/warranties_controller.rb
@@ -13,7 +13,11 @@ class Api::V1::WarrantiesController < ApplicationController
   def edit
     @warranty.update(warranty_params)
     @warranty.save
+  end
 
+  def create
+    warranty = Warranty.create(warranty_params)
+    warranty.save
   end
 
   private
@@ -22,6 +26,6 @@ class Api::V1::WarrantiesController < ApplicationController
     end
 
     def warranty_params
-      params.require(:warranty).permit(:expiration)
+      params.require(:warranty).permit(:name, :category, :wrnty_days, :buy_date, :notes, :photo_id, :user_id, :expiration)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       patch '/warranties/:id', to: 'warranties#edit'
+      post '/warranties', to: 'warranties#create'
       resources :warranties
       resources :users, only: [:create]
       post '/login', to: 'auth#create'
